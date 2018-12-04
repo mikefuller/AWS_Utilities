762b531b8cf2add04a383928746d071cee96fa4a
Mike Fuller <mike.s.fuller@gmail.com>
2018-12-04 14:55:40 -0500

    Update InventoryCompare.py

--- EC2 Inventory Comparison/InventoryCompare.py
+++ EC2 Inventory Comparison/InventoryCompare.py
@@ -1,22 +1,4 @@
 #!/usr/bin/env python
-########################################################################
-#
-# Copyright (c) 2015, Rean Cloud, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#-----------------------------------------------------------------------
-# Author : Rean Cloud, Inc.
 ############################# DESCRIPTION ##############################
 #
 # Script will take a monthly inventory and compare it against the previous
@@ -44,11 +26,11 @@ from email.mime.text import MIMEText
 # Variables
 past_inventory = 'past_inventory.csv'
 current_inventory = 'current_inventory.csv'
-aws_region = 'us-gov-west-2'
+aws_region = 'us-west-2'
 useProxy = False
 proxyIP = ''
 env = 'Test'
-email_distro_list = 'mike.fuller@sapns2.com'
+email_distro_list = 'me@email.com'
 smtp_server = 'localhost'
 
 # Logging configuration
